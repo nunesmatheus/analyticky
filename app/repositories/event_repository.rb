57f0767d27262997c67c1a7ac2ca4d32648c044d
Matheus Nunes <matheus.o.nunes@gmail.com>
2020-12-06 11:21:32 -0300

    Fix index name for alternative environments

--- app/repositories/event_repository.rb
+++ app/repositories/event_repository.rb
@@ -6,7 +6,12 @@ class EventRepository
   include Elasticsearch::Persistence::Repository
   include Elasticsearch::Persistence::Repository::DSL
 
-  index_name "events_#{Time.zone.today}"
+  if Rails.env.production?
+    index_name "events_#{Time.zone.today}"
+  else
+    index_name "#{Rails.env}_events_#{Time.zone.today}"
+  end
+
   klass Event
 
   client Elasticsearch::Client.new(
