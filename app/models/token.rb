50c71573b2e21f9076c20c0d8af915a4b0fdcc9a
Matheus Nunes <matheus.o.nunes@gmail.com>
2021-01-15 17:42:39 -0300

    Fix encryption secret

--- app/models/token.rb
+++ app/models/token.rb
@@ -2,7 +2,8 @@
 
 class Token < ApplicationRecord
   def value
-    secret = Rails.application.secrets.secret_key_base[0..31]
+    secret = Rails.application.secrets.secret_key_base || ENV['SECRET_KEY_BASE']
+    secret = secret[0..31]
     crypt = ActiveSupport::MessageEncryptor.new(secret)
     crypt.encrypt_and_sign(id)
   end
